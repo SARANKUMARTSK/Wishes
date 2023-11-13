a271845ea50ceabed0ea738d4dc89b02db80fcde
SARANKUMARTSK <sarankumartsk@gmail.com>
2023-11-13 10:29:46 +0530

    The File Submitted

---
+++ script.js
@@ -0,0 +1,56 @@
+setTimeout(() => {
+    console.log(10);
+    let value = document.getElementById("countown");
+    value.innerHTML = "10";
+}, 1000)
+setTimeout(() => {
+    console.log(9);
+    value = document.getElementById("countown");
+    value.innerHTML = "9";
+}, 2000)
+setTimeout(() => {
+    console.log(8);
+    value = document.getElementById("countown");
+    value.innerHTML = "8";
+}, 3000)
+setTimeout(() => {
+    console.log(7);
+    value = document.getElementById("countown");
+    value.innerHTML = "7";
+}, 4000)
+setTimeout(() => {
+    console.log(6);
+    value = document.getElementById("countown");
+    value.innerHTML = "6";
+}, 5000)
+setTimeout(() => {
+    console.log(5);
+    value = document.getElementById("countown");
+    value.innerHTML = "5";
+}, 6000)
+setTimeout(() => {
+    console.log(4);
+    value = document.getElementById("countown");
+    value.innerHTML = "4";
+}, 7000)
+
+setTimeout(() => {
+    console.log(3);
+    value = document.getElementById("countown");
+    value.innerHTML = "3";
+}, 8000)
+setTimeout(() => {
+    console.log(2);
+    value = document.getElementById("countown");
+    value.innerHTML = "2";
+}, 9000)
+setTimeout(() => {
+    console.log(1);
+    value = document.getElementById("countown");
+    value.innerHTML = "1";
+}, 10000)
+setTimeout(() => {
+    console.log("Happy Independance Day");
+    value = document.getElementById("countown");
+    value.innerHTML = "Happy Independance Day🥳👏🏻";
+}, 11000)
